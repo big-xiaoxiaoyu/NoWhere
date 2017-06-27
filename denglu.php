75b764fb6d575e3e714c07593c38eeb75993a2c5
gaoxiang099 <472944822@qq.com>
2017-06-27 13:19:27 +0800

    登录
    登录

---
+++ denglu.php
@@ -0,0 +1,119 @@
+<?php require_once('../Connections/aa.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_aa, $aa);
+$query_Recordset1 = "SELECT * FROM bb";
+$Recordset1 = mysql_query($query_Recordset1, $aa) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+?>
+<?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['textfield'])) {
+  $loginUsername=$_POST['textfield'];
+  $password=$_POST['textfield2'];
+  $MM_fldUserAuthorization = "";
+  $MM_redirectLoginSuccess = "../chenggong.php";
+  $MM_redirectLoginFailed = "../shibai.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_aa, $aa);
+  
+  $LoginRS__query=sprintf("SELECT username, password FROM bb WHERE username=%s AND password=%s",
+    GetSQLValueString($loginUsername, "int"), GetSQLValueString($password, "int")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $aa) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+     $loginStrGroup = "";
+    
+	if (PHP_VERSION >= 5.1) {session_regenerate_id(true);} else {session_regenerate_id();}
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>无标题文档</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<form id="form1" name="form1" method="POST" action="<?php echo $loginFormAction; ?>">
+  <p align="center">&nbsp;</p>
+  <p align="center">&nbsp;</p>
+  <p align="center">&nbsp;</p>
+  <p align="center">&nbsp;</p>
+  <p align="center">登陆</p>
+  <p align="center">账号
+    <label for="textfield"></label>
+    <input type="text" name="textfield" id="textfield" />
+  </p>
+  <p align="center">密码
+    <label for="textfield2"></label>
+    <input type="password" name="textfield2" id="textfield2" />
+  </p>
+  <div align="center">
+    <p>
+      <input name="登录" type=submit id="登录" value="登录">
+    </p>
+  </div>
+</form>
+</body>
+</html>
+<?php
+mysql_free_result($Recordset1);
+?>
